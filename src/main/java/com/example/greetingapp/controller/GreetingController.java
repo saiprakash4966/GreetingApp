df320cc4b15fafa3eb14734a43c007198734f278
Your Name <you@example.com>
2022-05-20 12:14:52 +0530

    Displaying Greeting Fullname(First and last name)

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -17,10 +17,35 @@ public class GreetingController {
     @Autowired
     private IGreetingService iGreetingService;
 
+    /**
+     * Method using GET to pass the name to the User and then to the Service layer.
+     * @param name - Will pass the name
+     * @return - Will return the counter and user name
+     * URL : http://localhost:8081/greeting/home?name=Tom
+     */
     @GetMapping("/home")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         User user = new User();
         user.setFirstName(name);
         return iGreetingService.addGreeting(user);
     }
+
+    /**
+     *  Method using Get to pass the firstname and lastname and set it using the setters
+     *  Then the call the service layer and add the user to repository
+     * @param firstName
+     * @param lastName
+     * @return
+     * URL : http://localhost:8081/greeting/fullname?firstName=Tom&lastName=Thomas
+     */
+    @GetMapping("/fullname")
+    public Greeting greeting(@RequestParam(value = "firstName", defaultValue = "first") String firstName,
+                             @RequestParam(value = "lastName", defaultValue = "last") String lastName ) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return iGreetingService.addGreeting(user);
+    }
+
+
 }

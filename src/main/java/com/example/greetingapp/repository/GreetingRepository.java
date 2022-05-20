020be00545b0267a55e898ffd990ecd909e3ee6c
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-05-20 18:12:25 +0530

    Delete GreetingRepository.java

--- src/main/java/com/example/greetingapp/repository/GreetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.greetingapp.repository;
-
-import com.example.greetingapp.entity.Greeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingRepository extends JpaRepository<Greeting,Long> {
-}
